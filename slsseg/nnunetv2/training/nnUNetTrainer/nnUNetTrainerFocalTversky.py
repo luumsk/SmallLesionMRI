b40b6a4b52a6e22c0bb1d63637af51fe321a4405
khue <luumsk@gmail.com>
2026-03-19 16:00:42 +0700

    add FocalTversky trainer

---
+++ slsseg/nnunetv2/training/nnUNetTrainer/nnUNetTrainerFocalTversky.py
@@ -0,0 +1,52 @@
+from monai.losses import TverskyLoss
+from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+import numpy as np
+import torch
+
+
+class FocalTverskyLoss(torch.nn.Module):
+    def __init__(self, alpha: float, beta: float, gamma: float):
+        super().__init__()
+        self.gamma = gamma
+        self.tversky_loss = TverskyLoss(
+            to_onehot_y=True,
+            softmax=True,
+            alpha=alpha,
+            beta=beta,
+            reduction="mean",
+        )
+
+    def forward(self, y_hat, y):
+        tversky_loss = self.tversky_loss(y_hat, y)
+        return torch.pow(tversky_loss, 1.0 / self.gamma)
+
+
+class nnUNetTrainerFocalTversky(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        
+        """nnU-Net trainer using focal Tversky-style loss."""
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        
+        self.initial_lr = 1e-2
+        self.num_epochs = 150
+        self.save_every = 5
+        self.disable_checkpointing = False
+    
+    def _build_loss(self):
+        loss = FocalTverskyLoss(alpha=0.3, beta=0.7, gamma=4.0 / 3.0)
+
+        deep_supervision_scales = self._get_deep_supervision_scales()
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+        weights[-1] = 0
+
+        # we don't use the lowest output. Normalize weights so that they sum to 1
+        weights = weights / weights.sum()
+        # now wrap the loss
+        loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
