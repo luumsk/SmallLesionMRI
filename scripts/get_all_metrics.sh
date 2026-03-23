3bf2fc3a7494fa5c9e5bf24a7750ea469b6233ee
khue <luumsk@gmail.com>
2026-03-24 00:04:55 +0700

    update paths for get_all_metrics.sh

--- scripts/get_all_metrics.sh
+++ scripts/get_all_metrics.sh
@@ -1,3 +1,3 @@
 python get_all_metrics.py \
-  -i metrics/MSLesSeg/latest_checkpoints \
-  -o metrics/MSLesSeg/latest_checkpoints/all_metrics.csv
+  -i "/media/storage/luu/SmallLesionMRI/metrics/MSLesSeg/main" \
+  -o "all_metrics.csv"
